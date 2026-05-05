9a826dc2c7a45200c91891b92e8b1ac14891368d
jirayutsuksakorn <jirayutsuksakorn@gmail.com>
2026-05-05 15:20:23 +0700

    html-jsx-v.3

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,23 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import App from "./App.jsx";
-// import { MessageProvider } from "./contexts/messageContext/MessageProvider";
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <MessageProvider>
-//       <App />
-//     </MessageProvider>
-//   </StrictMode>
-// );
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import './assets/css/App.css';
-import AppTest from "./AppTest";
+import App from "./App.jsx";
+import { MessageProvider } from "./contexts/messageContext/MessageProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppTest />
+    <MessageProvider>
+      <App />
+    </MessageProvider>
   </StrictMode>
 );
+
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import './assets/css/App.css';
+// import AppTest from "./AppTest";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <AppTest />
+//   </StrictMode>
+// );
