ec7b164f951d899ff1b05a8eac557a601de0ee05
thanyaboonaumpong <thanyaboonaumpong@hotmail.com>
2026-05-16 21:50:12 +0700

    Update ProductDetailPage.jsx
    แก้ products
    เป็น Products
    ให้หาไฟล์เจอ

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useState } from 'react'; 
-import { allProducts } from '../components/products';
+import { allProducts } from '../components/Products';
 
 const ProductDetailPage = () => {
   const { productId } = useParams();
